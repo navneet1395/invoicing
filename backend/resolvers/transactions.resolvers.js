3a6766515b1e9eb9fba1c5f0cc4c3c19eb2cd31b
Navneet Gupta <nnvkg107@gmail.com>
2024-04-14 18:43:55 +0530

    Transaction Resolver all mutation and query written

--- backend/resolvers/transactions.resolvers.js
+++ backend/resolvers/transactions.resolvers.js
@@ -1,15 +1,62 @@
+import Transaction from "../models/transaction.model.js";
 const transactionResolvers = {
-    Query: {
-        transactions: async (parent, args, context, info) => {
-            return await getTransactions(args, context, info)
+  Query: {
+    transactions: async (parent, args, context, info) => {
+      try {
+        if (!context.getUser()) {
+          throw new Error("Unauthorized");
+        }
+        const userId = await context.getUser()._id;
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    transaction: async (parent, args, context, info) => {
+      try {
+        const transaction = await Transaction.findById(args.transactionId);
+        if (!transaction) {
+          throw new Error("Transaction not found");
+        }
+        return transaction;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    createTransaction: async (parent, {input}, context, info) => {
+      try{
+        const userId = await context.getUser()._id; m 
+        const newTransaction = new Transaction({...input,userId:userId})
+        await newTransaction.save()
+        return newTransaction;
+
+      }catch (err) {
+        console.log("Error Creating new Transaction ",err)
+        throw new Error("Error creating transaction ")
+      }
+    },
+    updateTransaction: async(parent,{input},context,info)=>{
+        try{
+            const updateTransaction = await Transaction.findByIdUpdate(input.transactionId,input,{new:true})
+        return updateTransaction
+        }catch{
+            console.log("Error  in Updating Transaction ",err)
+            throw new Error("Errror in Updating Transaction")
         }
     },
-    Mutation: {
-        createTransaction: async (parent, args, context, info) => {
-            return await createTransaction(args, context, info)
+    deleteTransaction : async (_,{tranactionId})=>{
+        try{
+            const deleteTransaction = await Transaction.findByIdDelete(transactionId)
+        return deleteTransaction
+        }catch{
+            console.log("Error  in Deleting Transaction ",err)
+            throw new Error("Errror in Deleting Transaction")
         }
     }
-}
+  },
+};
 
-export default transactionResolvers
-
+export default transactionResolvers;
